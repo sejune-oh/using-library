fe10cb70911a20cdd61b614f4b51051a91daac60
sejune-oh <etry0715@icloudhospital.com>
2023-02-13 16:25:20 +0900

    feat: Add translation locales files

--- src/pages/i18next.tsx
+++ src/pages/i18next.tsx
@@ -1,17 +1,58 @@
-import React from "react";
-const text =
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga ipsa ad vero sit veritatis mollitia deleniti inventore, doloribus consequuntur veniam. Odio, libero deserunt! Beatae, soluta voluptates magnam ex adipisci aliquid!";
+import languageOptions from "@/ resource/langOptions";
+import React, { useState } from "react";
+import { i18n, useTranslation } from "next-i18next";
 
-export default function I18next() {
+interface Props {
+  text: string;
+}
+
+export default function I18next({ text }: Props) {
+  const { t } = useTranslation("article");
+
+  const [value, setValue] = useState<string>(text);
+
+  const onChangeLangOptions = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang = e.target.value;
+    if (lang) i18n?.changeLanguage(lang);
+  };
+
   return (
     <>
       <div className="p-3">
-        <h1 className=" font-mono">i18next libraray </h1>
-        <div className="">
+        <div className="flex justify-start items-center gap-[1rem]">
+          <h1 className=" font-mono mb-4">i18next libraray </h1>
+          <select
+            className="border border-solid border-black p-1"
+            onChange={onChangeLangOptions}
+          >
+            {languageOptions
+              ? languageOptions.map((lang, idx) => {
+                  return (
+                    <option
+                      key={idx}
+                      value={lang.code}
+                    >{`${lang.code.toUpperCase()} (${
+                      lang.localizedName
+                    })`}</option>
+                  );
+                })
+              : null}
+          </select>
+        </div>
+        <div className="flex flex-col gap-[4px]">
           <h2>Text</h2>
-          <article></article>
+          <article>{t(value)}</article>
         </div>
       </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const text =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima minus quae aliquid in amet pariatur praesentium earum corrupti molestiae quidem voluptatum fuga voluptates nam, ratione vitae vero quasi? Odit, expedita?";
+
+  return {
+    props: { text },
+  };
 }
