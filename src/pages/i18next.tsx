4aa5e22eac17623c3642e0aada67dc0a2c2375c1
sejune-oh <etry0715@icloudhospital.com>
2023-02-13 14:21:44 +0900

    lib: Add library list:next-i18next, react-i18next i18next

--- src/pages/i18next.tsx
+++ src/pages/i18next.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+const text =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga ipsa ad vero sit veritatis mollitia deleniti inventore, doloribus consequuntur veniam. Odio, libero deserunt! Beatae, soluta voluptates magnam ex adipisci aliquid!";
 
 export default function I18next() {
   return (
-    <div>i18next</div>
-  )
+    <>
+      <div className="p-3">
+        <h1 className=" font-mono">i18next libraray </h1>
+        <div className="">
+          <h2>Text</h2>
+          <article></article>
+        </div>
+      </div>
+    </>
+  );
 }
