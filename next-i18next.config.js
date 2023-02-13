65e1bfccac64186c52e56342ceb8047b385a950d
sejune-oh <etry0715@icloudhospital.com>
2023-02-13 14:11:00 +0900

    feat: Implements  i18next.config.js

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const locales = require('./locales')
+
+const config = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: locales,
+    localeDetection: false,
+  }
+}
+
+module.exports = config
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <h1>Library Page  </h1>
+      </main>
     </>
   );
 }
